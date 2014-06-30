daa0c9db0bd38c2f154234a190759856065f072e
Chris Moyer <chris.moyer@gmail.com>
2014-06-29 21:09:50 -0400

    Initial Commit

---
+++ src/main/java/com/moecode/model/Shorty.java
@@ -0,0 +1,8 @@
+package com.moecode.model;
+
+public class Shorty {
+    private String shorty;
+    private String url;
+
+
+}
---
+++ src/main/java/com/moecode/service/SimpleShortyService.java
@@ -0,0 +1,7 @@
+package com.moecode.service;
+
+/**
+ * Created by moe on 6/29/14.
+ */
+public class SimpleShortyService {
+}
